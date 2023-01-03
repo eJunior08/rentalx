ee5f600b08bb50bc86055a722ac941e611a1c825
Elson Oliveira <elsonfojr@gmail.com>
2023-01-03 07:58:30 -0300

    feat: authentication use case created

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,51 @@
+import { inject, injectable } from "tsyringe";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from "../../entities/User";
+
+interface IRequestDTO {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: Pick<User, "email" | "name">;
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequestDTO): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "d652eeeea9a382e2b37ad73e0a66b131", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+  }
+}
