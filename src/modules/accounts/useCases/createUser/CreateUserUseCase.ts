34cf29bd7c4421675f414d37d023026bf609d1dc
Elson Oliveira <elsonfojr@gmail.com>
2023-01-03 07:57:27 -0300

    feat: use case for user created

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,26 @@
+import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(data: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      data.email
+    );
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(data.password, 8);
+    await this.usersRepository.create({ ...data, password: passwordHash });
+  }
+}
