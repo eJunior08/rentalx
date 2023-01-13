c24e8a489aeba94a70e2963022cf2bbdb8c73ebb
Elson Oliveira <elsonfojr@gmail.com>
2023-01-12 23:35:22 -0300

    test: authenticate test implemented

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,28 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create(data: ICreateUserDTO): Promise<void> {
+    const newUser = new User();
+
+    Object.assign(newUser, {
+      driver_license: data.driver_license,
+      email: data.email,
+      name: data.name,
+      password: data.password,
+    });
+
+    this.users.push(newUser);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
